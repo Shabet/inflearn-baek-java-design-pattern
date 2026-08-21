c2ff6e5c91b7e950decc1ad0351f415571cf82eb
Shabet <pakjunyoung@nate.com>
2026-08-21 17:37:09 +0900

    [p19 observer] ObserverInspring 컴파일 오류 수정

--- src/main/java/d03_behavioral_patterns/p19_observer/s03_java/ObserverInSpring.java
+++ src/main/java/d03_behavioral_patterns/p19_observer/s03_java/ObserverInSpring.java
@@ -3,9 +3,10 @@ package d03_behavioral_patterns.p19_observer.s03_java;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class ObserverInSpring {
 
     public static void main(String[] args) {
